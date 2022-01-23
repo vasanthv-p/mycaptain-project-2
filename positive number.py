fb65998bbeeecdfd6c1ba43b7cbb75bae22d31b5
vasanthv-p <98248194+vasanthv-p@users.noreply.github.com>
2022-01-23 13:41:46 +0530

    mycap

---
+++ positive number.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 12:22:31 2022
+
+@author: Asus-2020
+"""
+list1 = [12, -7, 5, 64, -14]
+for num in list1:
+    if num >= 0:
+       print(num, end = " ")
+      
